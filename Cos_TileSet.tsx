f333c4c9bdc770fd135a9bb9324d3caf3ed00377
PierreGalach <81149966+PierreGalach@users.noreply.github.com>
2021-03-22 10:26:56 +0100

    Delete Cos_TileSet.tsx

--- Cos_TileSet.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Cos_TileSet" tilewidth="32" tileheight="32" tilecount="88" columns="8">
- <image source="OutscaleCOS.png" width="256" height="352"/>
- <tile id="81">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
